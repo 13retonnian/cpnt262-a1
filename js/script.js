8802db0aba55798714c3f2b51f62bc92dd91a3cb
Chriztoph3 <chriztoph@hotmail.com>
2021-10-17 17:59:09 -0600

    spooky button makes most things spooky

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 const button = document.querySelector("button");
 const htmlClass = document.querySelector("html");
+const autumnClass = document.getElementsByClassName("autumn-class");
 
 const spookyButton = function(event) {
   event.preventDefault();
-  htmlClass.classList.toggle("spookyClass");
+  htmlClass.classList.toggle("html-spooky");
+  for(let i = 0; i < autumnClass.length; i++)
+  {
+    autumnClass[i].classList.toggle("spooky-class")  
+  }
+  
 }
 
 button.addEventListener("click", spookyButton);
