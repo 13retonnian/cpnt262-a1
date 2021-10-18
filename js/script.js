f650dde6c98daaac07144c898c270567e6779bc4
Chriztoph3 <chriztoph@hotmail.com>
2021-10-17 19:58:20 -0600

    changed nav colour changes

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const button = document.querySelector("button");
 const htmlClass = document.querySelector("html");
 const wordsClass = document.getElementsByClassName("autumn-class");
 const imageGif = document.querySelector("img");
+const aClass = document.querySelectorAll("a");
 let castle = false;
 
 const spookyButton = function(event) {
@@ -18,8 +19,12 @@ const spookyButton = function(event) {
   } else {
     imageGif.src = "img/castle.gif";
     castle = true;
+  }  
+
+  for(let i = 0; i < aClass.length; i++)
+  {
+    aClass[i].classList.toggle("aClass");
   }
-  
 }
 
 button.addEventListener("click", spookyButton);
